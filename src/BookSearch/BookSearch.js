934291749c869b9ec6e48fd61d735826d7ba22f7
Vincent Minicozzi <vinicozzimincent@gmail.com>
2024-02-08 12:24:15 -0500

    update search option

--- src/BookSearch/BookSearch.js
+++ src/BookSearch/BookSearch.js
@@ -1,14 +1,22 @@
-import React, { useState } from 'react';
-import { View, TextInput, Button, FlatList, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, TextInput, FlatList, Text, TouchableOpacity } from 'react-native';
 
 const BookSearch = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [searchResults, setSearchResults] = useState([]);
+  const [selectedBook, setSelectedBook] = useState(null);
 
-  const searchBooks = async () => {
+  const searchBooks = async (text) => {
     try {
+      setSearchTerm(text); 
+
+      if (text === '') {
+        setSearchResults([]); 
+        return;
+      }
+
       const apiKey = 'AIzaSyDN5R0GHTM6PXsT9QzM8p1JToNsY-IKYi8';
-      const apiUrl = `https://www.googleapis.com/books/v1/volumes?q=${searchTerm}&key=${apiKey}`;
+      const apiUrl = `https://www.googleapis.com/books/v1/volumes?q=${text}&key=${apiKey}`;
 
       const response = await fetch(apiUrl);
       const data = await response.json();
@@ -22,24 +30,32 @@ const BookSearch = () => {
       console.error('Error searching for books:', error);
     }
   };
+  
+
+  const handleBookSelection = (book) => {
+    setSelectedBook(book);
+    console.log('Selected book:', book); 
+  };
+
 
   return (
     <View>
       <TextInput
         placeholder="Enter book title"
         value={searchTerm}
-        onChangeText={(text) => setSearchTerm(text)}
+        onChangeText={searchBooks} 
       />
-      <Button title="Search" onPress={searchBooks} />
 
       <FlatList
         data={searchResults}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => (
-          <View>
-            <Text>{item.volumeInfo.title}</Text>
-          </View>
-        )}
+            <TouchableOpacity onPress={() => handleBookSelection(item)}>
+              <View>
+                <Text>{item.volumeInfo.title}</Text>
+              </View>
+            </TouchableOpacity>
+          )}
       />
     </View>
   );
